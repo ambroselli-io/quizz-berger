79a4ee8235e257aa7af533a293881c084c422746
Arnaud AMBROSELLI <arnaudambroselli@pc72.home>
2021-10-03 15:05:58 +0200

    fixing stuff

--- api/scripts/migrations.js
+++ api/scripts/migrations.js
@@ -1,3 +1,5 @@
 require("../src/mongo");
 require("dotenv").config({ path: ".env" });
 const mongoose = require("mongoose");
+const UserObject = require("../src/models/user");
+const AnswerObject = require("../src/models/answer");
