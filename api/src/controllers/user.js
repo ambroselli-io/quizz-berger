3598ccf9fd3ca576013e69cb490627db8c3d7d1a
Arnaud AMBROSELLI <arnaudambroselli@pc72.home>
2021-10-03 15:12:49 +0200

    new build

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -14,12 +14,12 @@ const setCookie = (req, res, user) => {
   const token = jwt.sign({ _id: user._id }, config.SECRET, { expiresIn: JWT_MAX_AGE });
 
   const tokenConfig = {
-    // domain:
-    //   production.env.NODE_ENV === "development"
-    //     ? req.headers.origin
-    //     : req.headers.origin.includes("fr")
-    //     ? "quizz-du-berger.fr"
-    //     : "quizz-du-berger.com",
+    domain:
+      production.env.NODE_ENV === "development"
+        ? req.headers.origin
+        : req.headers.origin.includes("fr")
+        ? "quizz-du-berger.fr"
+        : "quizz-du-berger.com",
     maxAge: JWT_MAX_AGE,
     httpOnly: process.env.NODE_ENV !== "development",
     secure: process.env.NODE_ENV !== "development",
