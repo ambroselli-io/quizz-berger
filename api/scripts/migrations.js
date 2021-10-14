14403d056d8bf2495b0d5a860e3541102e79ff88
Arnaud AMBROSELLI <arnaudambroselli@pc72.home>
2021-10-14 10:21:11 +0200

    new build

--- api/scripts/migrations.js
+++ api/scripts/migrations.js
@@ -1,5 +1,27 @@
 require("../src/mongo");
 require("dotenv").config({ path: ".env" });
 const mongoose = require("mongoose");
+const quizz = require("../data/quizz.json");
 const UserObject = require("../src/models/user");
 const AnswerObject = require("../src/models/answer");
+
+const questions = quizz.reduce((questions, theme) => [...questions, ...theme.questions], []);
+
+const fiveAnswersScores = [
+  [5, 3, 2, 0, 0, 0],
+  [4, 5, 3, 1, 0, 0],
+  [2, 3, 5, 3, 2, 0],
+  [0, 1, 3, 5, 4, 0],
+  [0, 0, 2, 3, 5, 0],
+  [0, 0, 0, 0, 0, 0],
+];
+
+// (async () => {
+//   for (const question of questions) {
+//     if (question.answers.length === 6) {
+//       if (JSON.stringify(question.scores) !== JSON.stringify(fiveAnswersScores)) {
+//         console.log(question.fr);
+//       }
+//     }
+//   }
+// })();
--- app/src/components/RadarChart.js
+++ app/src/components/RadarChart.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { Chart, RadarController, RadialLinearScale, Legend } from "chart.js";
