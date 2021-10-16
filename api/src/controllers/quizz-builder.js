ce49b426b2b3521267cd82c532836ca4dce83e21
Arnaud AMBROSELLI <arnaudambroselli@pc72.home>
2021-10-16 14:39:08 +0200

    a lot more questions

--- api/src/controllers/quizz-builder.js
+++ api/src/controllers/quizz-builder.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 const { catchErrors } = require("../utils/error");
+const stringify = require("json-stringify-pretty-compact");
+const mongoose = require("mongoose");
 
 router.get(
   "/:themeIndex/:questionIndex",
@@ -25,12 +27,14 @@ router.get(
       .replace("{{CURRENT_QUESTION}}", fr)
       .replace(
         "{{ALL_QUESTIONS}}",
-        quizz.map(
-          (theme, index) =>
-            `<h3>${index + 1}: ${theme.fr}</h3><ol>${theme.questions.map(
-              (q, qIndex) => `<li class="question" data-theme="${index}" data-question="${qIndex}">${q.fr}</li>`
-            )}</ol>`
-        )
+        quizz
+          .map(
+            (theme, index) =>
+              `<h3>${index + 1}: ${theme.fr}</h3><ol>${theme.questions
+                .map((q, qIndex) => `<li class="question" data-theme="${index}" data-question="${qIndex}">${q.fr}</li>`)
+                .join("<br />")}</ol>`
+          )
+          .join("")
       )
       .replace('<ol id="answers"></ol>', `<ol id="answers">${answers.map((a) => `<li><input maxlength="140" value="${a}" /></li>`).join("")}</ol>`)
       .replace("{{QUESTIONS_HEADER_ROW}}", `<th></th>${answers.map((a) => `<th>${a}</th>`).join("")}`)
@@ -55,6 +59,91 @@ router.get(
 );
 
 router.post(
+  "/new-theme",
+  catchErrors(async (req, res) => {
+    if (process.env.NODE_ENV !== "development") return res.status(200).send("forbidden");
+
+    const { currentThemeIndex } = req.body;
+
+    const quizz = JSON.parse(fs.readFileSync(path.resolve("./data/quizz.json"), "utf8"));
+
+    const newQuizz = quizz.reduce((themes, theme, index) => {
+      themes.push(theme);
+      if (index !== currentThemeIndex) return themes;
+      themes.push({
+        _id: `theme-${mongoose.Types.ObjectId()}`,
+        fr: "Nouveau thème",
+        questions: [
+          {
+            _id: `question-${mongoose.Types.ObjectId()}`,
+            fr: "Nouvelle question",
+            answers: ["Réponse 1", "Réponse 2", "Réponse 3", "Réponse 4", "Réponse 5", "Ça ne m'intéresse pas / Je n'ai pas d'avis"],
+            scores: [
+              [5, 3, 2, 0, 0, 0],
+              [4, 5, 3, 1, 0, 0],
+              [2, 3, 5, 3, 2, 0],
+              [0, 1, 3, 5, 4, 0],
+              [0, 0, 2, 3, 5, 0],
+              [0, 0, 0, 0, 0, 0],
+            ],
+          },
+        ],
+      });
+      return themes;
+    }, []);
+
+    console.log(stringify(newQuizz));
+
+    fs.writeFileSync(path.resolve("./data/quizz.json"), stringify(newQuizz));
+
+    res.status(200).send({ ok: true });
+  })
+);
+
+router.post(
+  "/new-question",
+  catchErrors(async (req, res) => {
+    if (process.env.NODE_ENV !== "development") return res.status(200).send("forbidden");
+
+    const { currentThemeIndex, currentQuestionIndex } = req.body;
+
+    const quizz = JSON.parse(fs.readFileSync(path.resolve("./data/quizz.json"), "utf8"));
+
+    const newQuizz = quizz.map((theme, index) => {
+      if (index !== currentThemeIndex) return theme;
+      return {
+        ...theme,
+        questions: theme.questions.reduce((questions, question, index) => {
+          questions.push(question);
+          if (index === currentQuestionIndex) {
+            questions.push({
+              _id: `question-${mongoose.Types.ObjectId()}`,
+              fr: "Nouvelle question",
+              answers: ["Réponse 1", "Réponse 2", "Réponse 3", "Réponse 4", "Réponse 5", "Ça ne m'intéresse pas / Je n'ai pas d'avis"],
+              scores: [
+                [5, 3, 2, 0, 0, 0],
+                [4, 5, 3, 1, 0, 0],
+                [2, 3, 5, 3, 2, 0],
+                [0, 1, 3, 5, 4, 0],
+                [0, 0, 2, 3, 5, 0],
+                [0, 0, 0, 0, 0, 0],
+              ],
+            });
+          }
+          return questions;
+        }, []),
+      };
+    });
+
+    console.log(stringify(newQuizz));
+
+    fs.writeFileSync(path.resolve("./data/quizz.json"), stringify(newQuizz));
+
+    res.status(200).send({ ok: true });
+  })
+);
+
+router.post(
   "/:themeIndex/:questionIndex",
   catchErrors(async (req, res) => {
     if (process.env.NODE_ENV !== "development") return res.status(200).send("forbidden");
@@ -68,7 +157,7 @@ router.post(
     quizz[req.params.themeIndex || 0].questions[req.params.questionIndex || 0].answers = body.answers;
     quizz[req.params.themeIndex || 0].questions[req.params.questionIndex || 0].scores = body.scores;
 
-    fs.writeFileSync(path.resolve("./data/quizz.json"), JSON.stringify(quizz, null, 2));
+    fs.writeFileSync(path.resolve("./data/quizz.json"), stringify(quizz));
 
     res.status(200).send({ ok: true });
   })
