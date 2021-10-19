eef8506c3907b5b03296689e04601efd6eeadc1a
Arnaud AMBROSELLI <arnaudambroselli@pc72.home>
2021-10-19 12:27:38 +0200

    clean

--- app/src/scenes/ThemeSelect.js
+++ app/src/scenes/ThemeSelect.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router";
 import { media } from "../styles/mediaQueries";
