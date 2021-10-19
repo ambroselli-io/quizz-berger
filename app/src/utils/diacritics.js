3924d630100a9bf673ff95161160f3ee41838bc2
Arnaud AMBROSELLI <arnaudambroselli@pc72.home>
2021-10-19 21:53:16 +0200

    clean

--- app/src/utils/diacritics.js
+++ app/src/utils/diacritics.js
@@ -224,6 +224,7 @@ for (let i = 0; i < defaultDiacriticsRemovalMap.length; i++) {
 }
 
 // "what?" version ... http://jsperf.com/diacritics/12
+// eslint-disable-next-line no-control-regex
 const removeDiacritics = (str) => str.replace(/[^\u0000-\u007E]/g, (a) => diacriticsMap[a] || a);
 
 export const normalizeWord = (word) => {
