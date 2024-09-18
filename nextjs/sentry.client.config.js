c7fc7e24eae51a51a22b69fd8132f296be625b5c
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-09-18 05:37:26 +0200

    sentry limit

--- nextjs/sentry.client.config.js
+++ nextjs/sentry.client.config.js
@@ -7,7 +7,7 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_URL,
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.001,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
